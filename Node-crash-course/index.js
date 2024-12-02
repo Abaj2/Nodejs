b915561412c9044e21b4637c8b01ff606e25ad81
Abaj2 <130621609+Abaj2@users.noreply.github.com>
2024-12-02 19:33:53 +1100

    Delete Node-crash-course directory

--- Node-crash-course/index.js
+++
@@ -1,3 +0,0 @@
-const { generateRandomNumber, celciusToFahrenheit } = require("./utils");
-console.log(`Random Number: ${generateRandomNumber()}`);
-console.log(`Celcius to fahrenheit: ${celciusToFahrenheit(32)}`);
