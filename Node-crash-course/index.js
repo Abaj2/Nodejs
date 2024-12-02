6a8dc014adc3b6442ca83ed55c4d82b5c7a1195a
Abaj2 <aaravbajaj08@gmail.com>
2024-12-02 19:34:55 +1100

    initial commit

---
+++ Node-crash-course/index.js
@@ -0,0 +1,10 @@
+<<<<<<< HEAD
+import getPosts, { getPostsLength } from "./postController.js";
+
+console.log(getPosts());
+console.log(`Posts Length: ${getPostsLength()}`);
+=======
+const { generateRandomNumber, celciusToFahrenheit } = require("./utils");
+console.log(`Random Number: ${generateRandomNumber()}`);
+console.log(`Celcius to fahrenheit: ${celciusToFahrenheit(32)}`);
+>>>>>>> ce10ce19366334e79553e36b015136d67f1ecdc6
---
+++ Node-crash-course/utils.js
@@ -0,0 +1,16 @@
+function generateRandomNumber() {
+<<<<<<< HEAD
+    return Math.floor(Math.random() * 100) + 1;
+  }
+  function celciusToFahrenheit(celcius) {
+    return (celcius * 9) / 5 + 32;
+  }
+  module.exports = { generateRandomNumber, celciusToFahrenheit };
+=======
+  return Math.floor(Math.random() * 100) + 1;
+}
+function celciusToFahrenheit(celcius) {
+  return (celcius * 9) / 5 + 32;
+}
+module.exports = { generateRandomNumber, celciusToFahrenheit };
+>>>>>>> ce10ce19366334e79553e36b015136d67f1ecdc6
