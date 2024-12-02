6566877b9e70e7afcc5758f9976ff5eae2dcddae
Aarav Bajaj <aaravbajaj08@gmail.com>
2024-12-02 14:32:30 +1100

    initial commit???

---
+++ Node-crash-course/index.js
@@ -0,0 +1,3 @@
+const { generateRandomNumber, celciusToFahrenheit } = require("./utils");
+console.log(`Random Number: ${generateRandomNumber()}`);
+console.log(`Celcius to fahrenheit: ${celciusToFahrenheit(32)}`);
