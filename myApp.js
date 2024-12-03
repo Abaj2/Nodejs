e1426ca61462c1b90e767311846ac37a642dcde5
Abaj2 <aaravbajaj08@gmail.com>
2024-12-03 19:52:03 +1100

    going to laptop so committing

--- myApp.js
+++ myApp.js
@@ -1,8 +1,22 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
 
-app.get("/", function(req, res) {
-    res.send("Hello Express");
+//app.get("/", function(req, res) {
+    //res.send("Hello Express");
+  //});
+  app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/views/index.html");
   });
-
+app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/json", (req, res) => {
+    let message = "Hello json"
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        message = message.toUpperCase();
+    }
+    res.send({
+        message
+    });
+});
  module.exports = app;
