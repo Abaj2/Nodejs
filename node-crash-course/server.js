a44a4bd7b9af476077c0f782f51aca38f27b8917
Abaj2 <aaravbajaj08@gmail.com>
2024-12-04 13:16:15 +1100

    commit

--- node-crash-course/server.js
+++ node-crash-course/server.js
@@ -1,13 +1,30 @@
-import http from 'http';
-const PORT = 8000
+import http from "http";
+const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/plain');
-    res.statusCode = 404;
-    res.end('<h1>Hello World!<h1/>');
-});
+  try {
+    // check if get request
+    if (req.method === "GET") {
+      if (req.url === "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>Homepage</h1>");
+      } else if (req.url === "/about") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>About</h1>");
+      } else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("<h1>Not Found</h1>");
+      }
+    } else {
+      throw new Error("Method not allwoed");
+    }
+  } catch (error) {
+        res.writeHead(500, { "Content-Type": "text/html" });
+        res.end("Server Error");
+      }
+  }
+);
 
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`);
 });
-
